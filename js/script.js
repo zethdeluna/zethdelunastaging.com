ce38c5d38ceeea81d126e736004fa3ae6efe3eac
zethdeluna <zethdeluna@users.noreply.github.com>
2021-11-26 11:42:36 -0800

    Projects page

--- js/script.js
+++ js/script.js
@@ -17,22 +17,22 @@ jQuery(function($) {
 
     // Fade In/Out
 
-    $(window).on('load', function() {
-        $(window).scroll(function() {
-          var windowBottom = $(this).scrollTop() + $(this).innerHeight();
-          $('.fade-in-out').each(function() {
-            /* Check the location of each desired element */
-            var objectBottom = $(this).offset().top + $(this).outerHeight();
+    // $(window).on('load', function() {
+    //     $(window).scroll(function() {
+    //       var windowBottom = $(this).scrollTop() + $(this).innerHeight();
+    //       $('.fade-in-out').each(function() {
+    //         /* Check the location of each desired element */
+    //         var objectBottom = $(this).offset().top + $(this).outerHeight();
             
-            /* If the element is completely within bounds of the window, fade it in */
-            if (objectBottom < windowBottom) { //object comes into view (scrolling down)
-              if ($(this).css('opacity')==0) {$(this).fadeTo(500,1);}
-            } else { //object goes out of view (scrolling up)
-              if ($(this).css('opacity')==1) {$(this).fadeTo(500,0);}
-            }
-          });
-        }).scroll(); //invoke scroll-handler on page-load
-      });
+    //         /* If the element is completely within bounds of the window, fade it in */
+    //         if (objectBottom < windowBottom) { //object comes into view (scrolling down)
+    //           if ($(this).css('opacity')==0) {$(this).fadeTo(500,1);}
+    //         } else { //object goes out of view (scrolling up)
+    //           if ($(this).css('opacity')==1) {$(this).fadeTo(500,0);}
+    //         }
+    //       });
+    //     }).scroll(); //invoke scroll-handler on page-load
+    //   });
 
     /**********Menu Button**********/
 
